acac9d11080b53add6cbda0ae5b9e7a00a856082
pleblira <91032628+pleblira@users.noreply.github.com>
2022-11-08 23:35:04 -0500

    removing .env

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,16 +18,14 @@ ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-g@nh0(ea^7+8i47d+)_(7otr3fx0b7+s=h7s^5h^*&p3u7o#53"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
